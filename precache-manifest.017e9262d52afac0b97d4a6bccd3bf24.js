1377309a28f0faf0972e4939955d4dac112502cd
ameerothman <arothman@wustl.edu>
2020-12-09 16:22:10 -0600

    Updates

---
+++ precache-manifest.017e9262d52afac0b97d4a6bccd3bf24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53d86ac51af439d8f6d8",
+    "url": "/09-todo-react-arothman11/static/css/main.1af24ae9.chunk.css"
+  },
+  {
+    "revision": "53d86ac51af439d8f6d8",
+    "url": "/09-todo-react-arothman11/static/js/main.53d86ac5.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/09-todo-react-arothman11/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "d391420aae786f32f1ea",
+    "url": "/09-todo-react-arothman11/static/js/runtime~main.d391420a.js"
+  },
+  {
+    "revision": "8c0d66c16ee71d4444b7d9849abe2d38",
+    "url": "/09-todo-react-arothman11/index.html"
+  }
+];
